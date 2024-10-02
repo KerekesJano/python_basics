a821631a859503dcdb769cb2fee7411f7ba7b253
Jano <kerekesjanos2005@gmail.com>
2024-10-02 13:20:30 +0200

    Learned about if statement.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python_basics
 Hello
 test
+test2
---
+++ app.py
@@ -0,0 +1,12 @@
+a: bool = True
+b: bool = False
+
+# Learn if statment
+
+""" if a:
+    print("a is True")
+
+elif b:
+    print("b is True")
+else:
+    print("neither a or b is True") """
